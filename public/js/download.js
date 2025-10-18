c8b8f44db385a0066ab2201ce462b6e9e164f0f8
Juan Oicata <jeotoicata@gmail.com>
2025-10-18 08:11:42 -0500

    Cambio en el link de descarga

--- public/js/download.js
+++ public/js/download.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Configuración de descarga desde GitHub Releases
-    const DOWNLOAD_URL = 'https://github.com/JuanOicata/SafePlay_Web/releases/download/v1.0.0/SafePlay-Setup-1.0.0.exe';
+    const DOWNLOAD_URL = 'https://github.com/JuanOicata/SafePlay_Web/releases/download/v1.0.0/win-unpacked.zip';
 
     // Manejar el botón de descarga principal
     const mainDownloadBtn = document.getElementById('mainDownloadBtn');
